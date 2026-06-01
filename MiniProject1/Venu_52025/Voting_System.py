3f9996922625b797985a372f23af2aeadef61841
Venu Gadekar <connectvenugadekar@gmail.com>
2026-06-01 11:49:32 +0530

    Add files via upload

---
+++ MiniProject1/Venu_52025/Voting_System.py
@@ -0,0 +1,38 @@
+# Voting System
+
+# Register voters
+voters = set()
+
+# Candidates and votes
+votes = { "Aman": 0, "Riya":0, "Rahul":0}
+
+n = int(input("Enter number of voters: "))
+
+# Cast votes
+for i in range(n):
+
+    voter_id = input("Enter voter ID: ")
+
+    # Duplicate prevention using set
+    if voter_id in voters:
+        print("Duplicate vote not allowed")
+        continue
+
+    voters.add(voter_id)
+
+    vote = input("Vote for Aman , Riya , Rahul : ")
+
+    if vote in votes:
+        votes[vote] += 1
+        print("Vote casted")
+    else:
+        print("Invalid candidate. Please enter the correct choice!!! ")
+
+# Tally result
+print("\nVote Count:")
+for candidate in votes:
+    print(candidate, "=", votes[candidate])
+
+# Declare winner
+winner = max(votes, key=votes.get)
+print("\nWinner is:", winner)
