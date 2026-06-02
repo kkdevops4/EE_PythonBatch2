9e74f79f534a195af6bedde1690b1da06fd0f7a2
Venu Gadekar <connectvenugadekar@gmail.com>
2026-06-02 09:54:30 +0530

    Voting_System.py

--- MiniProject1/Venu_52025/Voting_System.py
+++ MiniProject1/Venu_52025/Voting_System.py
@@ -4,7 +4,7 @@
 voters = set()
 
 # Candidates and votes
-votes = { "Aman": 0, "Riya":0, "Rahul":0}
+votes = {"Aman": 0, "Riya": 0, "Rahul": 0}
 
 n = int(input("Enter number of voters: "))
 
@@ -20,19 +20,29 @@ for i in range(n):
 
     voters.add(voter_id)
 
-    vote = input("Vote for Aman , Riya , Rahul : ")
+    vote = input("Vote for Aman, Riya, Rahul: ")
 
     if vote in votes:
         votes[vote] += 1
         print("Vote casted")
     else:
-        print("Invalid candidate. Please enter the correct choice!!! ")
+        print("Invalid candidate. Please enter the correct choice!!!")
 
 # Tally result
 print("\nVote Count:")
 for candidate in votes:
     print(candidate, "=", votes[candidate])
 
-# Declare winner
-winner = max(votes, key=votes.get)
-print("\nWinner is:", winner)
+# Check winner or tie
+max_votes = max(votes.values())
+
+winners = []
+
+for candidate in votes:
+    if votes[candidate] == max_votes:
+        winners.append(candidate)
+
+if len(winners) == 1:
+    print("\nWinner is:", winners[0])
+else:
+    print("\nIt's a tie between:" and ", ".join(winners))
